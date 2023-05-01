bcf2f1bf0363acff714dce085a67e747c7ec2932
ben-corbin <93767273+ben-corbin@users.noreply.github.com>
2023-05-01 09:39:22 +0100

    restore postcss & tailwind config files to module exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx,vue}",
-];
-export const theme = {
-  extend: {},
-  fontFamily: {
-    'sans': ['Plus Jakarta Text', 'sans-serif'],
-    'serif': ['Plus Jakarta Text', 'serif'],
-    'mono': ['Plus Jakarta Text', 'monospace'],
-    'display': ['Plus Jakarta Text', 'sans-serif'],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Plus Jakarta Text', 'sans-serif'],
+      'serif': ['Plus Jakarta Text', 'serif'],
+      'mono': ['Plus Jakarta Text', 'monospace'],
+      'display': ['Plus Jakarta Text', 'sans-serif'],
+    },
   },
-};
-export const plugins = [];
+  plugins: [],
+}
 
